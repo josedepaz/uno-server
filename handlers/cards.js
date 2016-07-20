2a7aac4f329be6e93dbc9aecc9fa5025139dfb1c
CORP Jose Manuel de Paz Estrada <jmpaz@gyt.com.gt>
2016-07-20 16:34:46 -0600

    MOD ...

--- handlers/cards.js
+++ handlers/cards.js
@@ -2,22 +2,22 @@
 
 const colors = ['red', 'green', 'blue', 'yellow'];
 const specials = [{ number: '+4' }, { number: '+4' }];
-let cards2 = [];
+let cards = [];
 for (var i = 0; i < 10; i++) {
     colors.forEach(function (c, j) {
-        cards2.push({ number: i, color: c });
+        cards.push({ number: i, color: c });
     });
 }
 
 colors.forEach(function (c, i) {
-    cards2.push({ number: "+2", color: c });
+    cards.push({ number: "+2", color: c });
 });
-cards2 = cards2.concat(specials);
+cards = cards.concat(specials);
 
 function random(low, high) {
     return Math.random() * (high - low) + low;
 }
 
 exports.getCard = function (request, reply) {
-    reply(cards2[Math.round(random(0,45))]);
+    reply(cards[Math.round(random(0,45))]);
 }
--- routes.js
+++ routes.js
@@ -25,7 +25,7 @@ module.exports = [
     },
     {
         method: 'GET',
-        path: '/cards/r',
+        path: '/cards/up',
         handler: Cards.getCard
     }
 ]
